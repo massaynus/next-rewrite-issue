824291cda69d9ae69301240a7e4c443811529ca3
Yassine Elmassaoudi <yassine.elmassaoudi_ext@payback.net>
2024-12-30 23:39:52 +0100

    format

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ export function middleware(req: NextRequest) {
     if (req.nextUrl.pathname.startsWith('/proxy')) {
         console.log('REQUEST:', req.url)
         console.warn('PATHNAME:', req.nextUrl.pathname);
+
         return NextResponse.rewrite(req.nextUrl.pathname.replace('/proxy', 'https://jsonplaceholder.typicode.com/todos'))
     }
 
